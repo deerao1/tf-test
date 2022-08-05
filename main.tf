66fcf5b7a3d5ee169a2da718fc544695e39c4f8f
deerao1 <drao28@gmail.com>
2022-08-05 11:06:01 +0530

    created

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  backend "s3" {
+    bucket = "zevopstest"
+    key = "terraform"
+    region = "us-east-2"
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-051dfed8f67f095f5"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
